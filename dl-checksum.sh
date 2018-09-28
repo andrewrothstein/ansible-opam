a7fa3aeb01c3d36b1d03c83e93205cb88c313bef
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-09-27 22:39:51 -0400

    up to v2.0.0. need to figure out the bwrap dependency

---
+++ dl-checksum.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+VER=2.0.0
+DIR=~/Downloads
+MIRROR=https://github.com/ocaml/opam/releases/download/$VER
+
+dl()
+{
+    PLATFORM=$1
+    OS=$2
+    F=opam-$VER-$PLATFORM-$OS
+    wget -O $DIR/$F $MIRROR/$F
+}
+
+dl arm64 linux
+dl armhf linux
+dl i686 linux
+dl x86_64 darwin
+dl x86_64 linux
+dl x86_64 openbsd
+
+sha256sum $DIR/opam-$VER-*
+
