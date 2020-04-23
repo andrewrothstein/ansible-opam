9df5d00cc0e380bd9f01bf485d585fb7e7e91bfc
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-04-23 13:24:38 -0400

    up to v2.0.7

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env sh
-VER=2.0.2
+set -e
 DIR=~/Downloads
-MIRROR=https://github.com/ocaml/opam/releases/download/$VER
+MIRROR=https://github.com/ocaml/opam/releases/download
 
 dl()
 {
-    PLATFORM=$1
-    OS=$2
-    F=opam-$VER-$PLATFORM-$OS
-    wget -O $DIR/$F $MIRROR/$F
+    local ver=$1
+    local arch=$2
+    local os=$3
+    local platform="${arch}-${os}"
+    local file="opam-${ver}-${platform}"
+    local lfile=$DIR/$file
+    local url=$MIRROR/$ver/$file
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{ print $1 }')
 }
 
-dl arm64 linux
-dl armhf linux
-dl i686 linux
-dl x86_64 darwin
-dl x86_64 linux
-dl x86_64 openbsd
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver arm64 linux
+    dl $ver armhf linux
+    dl $ver i686 linux
+    dl $ver x86_64 linux
+    dl $ver x86_64 macos
+    dl $ver x86_64 openbsd
+}
 
-sha256sum $DIR/opam-$VER-*
+dl_ver 2.0.5
+dl_ver 2.0.6
+dl_ver ${1:-2.0.7}
 
