95063e9585955726c9afc4f17d0aa990e02db64c
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-03-18 03:31:56 +0000

    opam:2.3.0 ocaml:5.3.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -8,13 +8,14 @@ dl()
     local ver=$1
     local arch=$2
     local os=$3
+    local dotexe=${4:-""}
     local platform="${arch}-${os}"
-    local file="opam-${ver}-${platform}"
+    local file="opam-${ver}-${platform}${dotexe}"
     local lfile=$DIR/$file
     local url=$MIRROR/$ver/$file
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o "${lfile}" "${url}"
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{ print $1 }')
@@ -26,9 +27,15 @@ dl_ver() {
     dl $ver arm64 linux
     dl $ver armhf linux
     dl $ver i686 linux
+    dl $ver ppc64le linux
+    dl $ver riscv64 linux
+    dl $ver s390x linux
     dl $ver x86_64 linux
+    dl $ver arm64 macos
+    dl $ver x86_64 netbsd
     dl $ver x86_64 macos
     dl $ver x86_64 openbsd
+    dl $ver x86_64 windows .exe
 }
 
-dl_ver ${1:-2.1.4}
+dl_ver ${1:-2.3.0}
